8ec664d428c466875751c9aac174bcc1954ac3f4
Mariia Dzhafarova <grinchak.1997@gmail.com>
2024-06-26 06:56:09 +0300

    add features

--- src/app/page/profile-page/welcome/welcome.component.ts
+++ src/app/page/profile-page/welcome/welcome.component.ts
@@ -11,6 +11,16 @@ import { Router } from '@angular/router';
 })
 export class WelcomeComponent {
 
+  public features = [
+    'Edit category by double click on category name (press Enter to save)',
+    'Add new category by click on "Add new category" input (press Enter to add)',
+    'Remove category with all todos by click on "Delete category" button',
+    'Add todos for each category by click on "Add" button',
+    'Edit todos by click on "edit" icon(press Enter to save)',
+    'Remove todos by click on "delete" icon',
+    'Search todos by title'
+  ];
+
   constructor(private authService: AuthService,
     private route: Router
   ) {
